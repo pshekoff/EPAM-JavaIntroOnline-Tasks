adfa67069e5167d250e0f87e68b8b060fa1f6566
Pavel <70515365+pshekoff@users.noreply.github.com>
2020-12-15 16:52:47 +0300

    Update SortingPractice5.java

--- javaIntro_2_Algorithmization/SortingPractice5.java
+++ javaIntro_2_Algorithmization/SortingPractice5.java
@@ -16,7 +16,7 @@ public class SortingPractice5 {
 			arr[i] = arr[i+1] + (int)ran;
 		}
 		
-		System.out.println("»сходный массив: " + Arrays.toString(arr));
+		System.out.println("Исходный массив: " + Arrays.toString(arr));
 		
 		for (int i = 0; i < arr.length; i++) {
 			int index = getIndex(arr, 0, i-1, arr[i]);
@@ -27,7 +27,7 @@ public class SortingPractice5 {
 			arr[index] = value;
 		}
 		
-		System.out.println("—ортировка вставками по возрастанию: " + Arrays.toString(arr));
+		System.out.println("Сортировка вставками по возрастанию: " + Arrays.toString(arr));
 
 	}
 	
