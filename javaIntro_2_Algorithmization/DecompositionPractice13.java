ae63cce176c99d5b78174400879f37e3d33d7967
Pavel <70515365+pshekoff@users.noreply.github.com>
2020-12-15 16:23:55 +0300

    Update DecompositionPractice13.java

--- javaIntro_2_Algorithmization/DecompositionPractice13.java
+++ javaIntro_2_Algorithmization/DecompositionPractice13.java
@@ -8,13 +8,13 @@ public class DecompositionPractice13 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("¬ведите n > 2");
+		System.out.println("Введите n > 2");
 		
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		scan.close();
 		
-		System.out.print("—писок простых чисел близнецов на отрезке [" + n + "," + 2*n + "]: " + Arrays.toString(getSimpleTwins(n)));
+		System.out.print("Список простых чисел близнецов на отрезке [" + n + "," + 2*n + "]: " + Arrays.toString(getSimpleTwins(n)));
 
 	}
 	
