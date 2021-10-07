b67263c6e20c89d0f7c2761607224e25b19dce88
Pavel <70515365+pshekoff@users.noreply.github.com>
2021-10-07 13:19:14 +0300

    Add files via upload

--- javaIntro_4_Classes/Part_1_Practice_02/Practice02_Main_Test2.java
+++ javaIntro_4_Classes/Part_1_Practice_02/Practice02_Main_Test2.java
@@ -1,9 +1,17 @@
 package javaIntro_4_Classes;
 
 public class Practice02_Main_Test2 {
+	
+	/* 2. Создайте класс Test2 с двумя переменными. Добавьте конструктор с входными параметрами.
+	 * Добавьте конструктор, инициализирующий члены класса по умолчанию. Добавьте set- и get-
+	 * методы для полей экземпляра класса.
+	 */
+	
 	public static void main(String[] args) {
+		
 		Test2 test = new Test2();
 		System.out.println("Variables by default are: " + test.getVar1() + " and " + test.getVar2());
+		
 		test.setVar1(49);
 		test.setVar2(169);
 		System.out.println("New varialbles are: "+ test.getVar1() + " and  " + test.getVar2());		
