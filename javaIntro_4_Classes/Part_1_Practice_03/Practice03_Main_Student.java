dd373c4976b8805067a3dd45c2b7ab6ba68678a2
Pavel <70515365+pshekoff@users.noreply.github.com>
2021-10-09 10:08:07 +0300

    Add files via upload

--- javaIntro_4_Classes/Part_1_Practice_03/Practice03_Main_Student.java
+++ javaIntro_4_Classes/Part_1_Practice_03/Practice03_Main_Student.java
@@ -3,7 +3,14 @@ package javaIntro_4_Classes;
 import java.util.Random;
 
 public class Practice03_Main_Student {
+	
+	/* 3. Создайте класс с именем Student, содержащий поля: фамилия и инициалы, номер группы, успеваемость
+	 * (массив из пяти элементов). Создайте массив из десяти элементов такого типа. Добавьте возможность
+	 * вывода фамилий и номеров групп студентов, имеющих оценки, равные только 9 или 10.
+	 */
+	
 	public static void main(String[] args) {
+		
 		Student[] studentList = new Student[10];
 		studentList[0] = new Student("Fetisov A.A.", 611101);
 		studentList[1] = new Student("Levsha N.F.", 611101);
@@ -16,6 +23,7 @@ public class Practice03_Main_Student {
 		studentList[8] = new Student("Fadin E.L.", 611104);
 		studentList[9] = new Student("Vilkova N.G.", 611104);
 		
+		//заполнение массива успеваемости студентов случайными оценками от 8 до 10
 		Random ran = new Random();
 		for (int i = 0; i < studentList.length; i++) {
 			for (int j = 0; j < 5; j++) {
@@ -23,11 +31,13 @@ public class Practice03_Main_Student {
 			}
 		}
 		
+		//вывод всех студентов
 		System.out.println("All students:");
 		for (int i = 0; i < studentList.length; i++) {
 			studentList[i].output();
 		}
 		
+		//вывод отличников
 		System.out.println("Excellent students:");
 		for (int i = 0; i < studentList.length; i++) {
 			if (studentList[i].isOK()) {
