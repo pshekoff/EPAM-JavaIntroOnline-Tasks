a6b671288469e3f5e32c5abc1073bd7c686c0261
Pavel <70515365+pshekoff@users.noreply.github.com>
2021-10-19 18:29:42 +0300

    Add files via upload

--- javaIntro_4_Classes/Part_2_Practice_05/Part2_Practice05_Main_Tours.java
+++ javaIntro_4_Classes/Part_2_Practice_05/Part2_Practice05_Main_Tours.java
@@ -6,7 +6,14 @@ import java.util.Collections;
 
 public class Part2_Practice05_Main_Tours {
 	
+	/* 5. Туристические путевки. Сформировать набор предложений клиенту по выбору туристической путевки
+	 * различного типа (отдых, экскурсии, лечение, шопинг, круиз и т.д.) для оптимального выбора.
+	 * Учитывать возможность выбора транспорта, питания и числа дней. Реализовать выбор и сортировку путевок.
+	 */
+	
 	public static void main(String[] args) {
+		
+		//список путевок
 		ArrayList<Tour> tours = new ArrayList<>();
 		tours.add(new Tour("Vacation", "Plane", false, 8, 700));
 		tours.add(new Tour("Vacation", "Plane", true, 10, 1050));
@@ -38,13 +45,13 @@ public class Part2_Practice05_Main_Tours {
 		tours.add(new Tour("Shopping", "Train", false, 3, 300));
 		tours.add(new Tour("Shopping", "Bus", false, 3, 250));
 		
-		System.out.println("Please, select a tour options:");
-		System.out.println("Choose a tour type (you can choose one or more options):"
-				+ "\n(1) Vacation"
-				+ "\n(2) Cruise"
-				+ "\n(3) Excurtions"
-				+ "\n(4) Therapy"
-				+ "\n(5) Shopping");
+		//выбор типа тура
+		System.out.println("Choose a tour type:"
+						+ "\n(1) Vacation"
+						+ "\n(2) Cruise"
+						+ "\n(3) Excurtions"
+						+ "\n(4) Therapy"
+						+ "\n(5) Shopping");
 		Scanner scan = new Scanner(System.in);
 		int typeOption = scan.nextInt();
 		String type = "";
@@ -64,14 +71,13 @@ public class Part2_Practice05_Main_Tours {
 			type = "Shopping";
 		}
 		
-		System.out.println("Type is: " + type);
-		System.out.println(type.equals(tours.get(0).getType()));
-		
+		//выбор транспорта
 		System.out.println("Choose a transport:"
-				+ "\n(1) by plane"
-				+ "\n(2) by train"
-				+ "\n(3) by bus");
+						+ "\n(1) by plane"
+						+ "\n(2) by train"
+						+ "\n(3) by bus");
 		int transportOption = scan.nextInt();
+		
 		String transport = "";
 		if (transportOption == 1) {
 			transport = "Plane";
@@ -83,21 +89,30 @@ public class Part2_Practice05_Main_Tours {
 			transport = "Bus";
 		}
 		
+		//включать ли питание
 		System.out.print("Include food? (1 - yes, 0 - no): ");
 		int foodOption = scan.nextInt();
+		
 		boolean food = false;
 		if (foodOption == 1) {
 			food = true;
 		}
+		else if (foodOption == 0) {
+			food = false;
+		}
 	
-		System.out.print("Enter a tour duration diapazon: ");
+		//выбор длительности тура
+		System.out.println("Enter a tour duration diapazon: ");
+		System.out.print("minimum days: ");
 		int durationMin = scan.nextInt();
+		System.out.print("maximum days: ");
 		int durationMax = scan.nextInt();
 		
 		scan.close();
 		
 		ArrayList<Tour> toursFiltered  = new ArrayList<>();
 		
+		//поиск туров по введенным параметрам
 		for (int i = 0; i < tours.size(); i++) {
 			if (type.equals(tours.get(i).getType())) {
 				if (transport.equals(tours.get(i).getTransport())) {
@@ -110,6 +125,7 @@ public class Part2_Practice05_Main_Tours {
 			}
 		}
 		
+		//вывод подходящих туров с сортировкой по стоимости
 		System.out.println("Please, see our tours offers for you:");
 		Collections.sort(toursFiltered, new Tour.CompareByCost());
 		for (int i = 0; i < toursFiltered.size(); i++) {
