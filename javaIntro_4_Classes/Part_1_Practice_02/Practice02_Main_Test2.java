72853038d81468a086893eb566c358aff15893a0
Pavel <70515365+pshekoff@users.noreply.github.com>
2021-10-19 18:14:18 +0300

    Add files via upload

--- javaIntro_4_Classes/Part_1_Practice_02/Practice02_Main_Test2.java
+++ javaIntro_4_Classes/Part_1_Practice_02/Practice02_Main_Test2.java
@@ -9,9 +9,11 @@ public class Practice02_Main_Test2 {
 	
 	public static void main(String[] args) {
 		
+		//объект класса Test2 с переменными по умолчанию
 		Test2 test = new Test2();
 		System.out.println("Variables by default are: " + test.getVar1() + " and " + test.getVar2());
 		
+		//установка новых значений переменных
 		test.setVar1(49);
 		test.setVar2(169);
 		System.out.println("New varialbles are: "+ test.getVar1() + " and  " + test.getVar2());		
--- javaIntro_4_Classes/Part_1_Practice_02/Test2.java
+++ javaIntro_4_Classes/Part_1_Practice_02/Test2.java
@@ -4,21 +4,27 @@ public class Test2 {
 	
 	private int var1, var2;
 	
+	//конструктор, инициализирующий члены класса по умолчанию
 	public Test2() {
 		this.var1 = 10;
 		this.var2 = 100;
 	}
+	
+	//конструктор с входными параметрами
 	public Test2(int var1, int var2) {
 		this.var1 = var1;
 		this.var2 = var2;
 	}
 	
+	//get-методы
 	public int getVar1() {
 		return var1;
 	}
 	public int getVar2() {
 		return var2;
 	}
+	
+	//set-методы
 	public void setVar1(int var) {
 		var1 = var;
 	}
