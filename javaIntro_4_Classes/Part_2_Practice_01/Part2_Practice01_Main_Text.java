3cb4ca18e5d9b9be2765e2657eb704b827350e4d
Pavel <70515365+pshekoff@users.noreply.github.com>
2021-10-19 18:25:37 +0300

    Add files via upload

--- javaIntro_4_Classes/Part_2_Practice_01/Part2_Practice01_Main_Text.java
+++ javaIntro_4_Classes/Part_2_Practice_01/Part2_Practice01_Main_Text.java
@@ -1,20 +1,38 @@
 package javaIntro_4_Classes;
 
 public class Part2_Practice01_Main_Text {
+	
+	// 1. Создать объект класса Текст, используя классы Предложение, Слово. Методы: дополнить текст,
+	// вывести на консоль текст, заголовок текста.
 
 	public static void main(String[] args) {
 	
+		//слово
 		Word w = new Word("test");
+		
+		//предложение
 		Sentence head = new Sentence("The new!");
+		
+		//добавление слов в предложение
 		head.addWord(w);
 		head.addWord("header");
+		
+		//текст с заголовком head
 		Text t = new Text(head);
+		
+		//содержание текста
 		t.setBody("First sentence!? Second sentence!");
+		
+		//добавление предложений
 		t.addBody("Third sentence");
 		Sentence s = new Sentence ("Last sentence");
 		t.addBody(s);
+		
+		//вывод заголовка
 		t.printHeader();
 		System.out.println();
+		
+		//вывод текста
 		t.printBody();
 		System.out.println();
 	}
